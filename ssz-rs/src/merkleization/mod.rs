b58448c30caa3cfc15bf696c020abb01001cac89
claravanstaden <Cats 4 life!>
2022-12-14 13:47:12 +0200

    Implements From impl from SerializeError to MerkleizationError::SerializationError.

--- ssz-rs/src/merkleization/mod.rs
+++ ssz-rs/src/merkleization/mod.rs
@@ -10,6 +10,7 @@ use sha2::{Digest, Sha256};
 pub use cache::Cache as MerkleCache;
 pub use node::Node;
 pub use proofs::is_valid_merkle_branch;
+use crate::SerializeError;
 
 pub(crate) const BYTES_PER_CHUNK: usize = 32;
 
@@ -23,6 +24,12 @@ pub enum MerkleizationError {
     SerializationError,           // failed to serialize value
     PartialChunk(Vec<u8>, usize), // cannot merkleize a partial chunk of length {1} (data: {0:?})
     InputExceedsLimit(usize),     // cannot merkleize data that exceeds the declared limit {0}
+}
+
+impl From<SerializeError> for MerkleizationError {
+    fn from(_: SerializeError) -> Self {
+        return MerkleizationError::SerializationError
+    }
 }
 
 impl Display for MerkleizationError {
@@ -60,9 +67,7 @@ where
 {
     let mut buffer = vec![];
     for value in values {
-        value
-            .serialize(&mut buffer)
-            .map_err(|_| MerkleizationError::SerializationError)?;
+        value.serialize(&mut buffer);
     }
     pack_bytes(&mut buffer);
     Ok(buffer)
